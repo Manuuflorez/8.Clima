369cf97f01a4da3dc22a58ea22cfb4c36dbb4c41
ManuelaF <Manuuflorez@hotmail.com>
2022-03-13 20:39:38 -0500

    Inicio

---
+++ src/pkg8/clima/Clima.java
@@ -0,0 +1,25 @@
+
+package pkg8.clima;
+/*Se tienen los registros que se hicieron del tiempo que hizo en la ciudad de 
+Medellín durante un año así (suponga 12 meses de 30 días cada uno): 
+Si hacía día soleado se registraba un cero (0)
+Si hacía día nublado se registraba un uno (1)
+Si hacía día lluvioso se registraba un dos (2)
+
+
+Se debe escribir un programa que Lea estos datos y con ellos forme un vector y 
+luego muestre el siguiente MENU:
+Número de días soleados
+Número de días nublados
+Número de días lluviosos
+Porcentaje de días soleados en relación a todo el mes
+Porcentaje de días nublados en relación a todo el mes
+Porcentaje de días lluviosos en relación a todo el mes*/
+
+public class Clima {
+
+    public static void main(String[] args) {
+
+    }
+    
+}
